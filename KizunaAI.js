f3233b61ecbc5b7755a9878636b0a60afc56096f
松本将輝 <Air@MacBook-Pro.local>
2018-01-01 02:39:31 +0900

    ダブルタップの制御

--- KizunaAI.js
+++ KizunaAI.js
@@ -12,3 +12,27 @@ function sound()
 	// [ID:sound-file]の音声ファイルを再生[play()]する
 	document.getElementById( id ).play() ;
 }
+
+/ダブルタップの挙動制御/ 
+(function(win, doc) {
+
+    "use strict";
+
+    var tapFlag = false,
+        timer;
+
+    doc.body.addEventListener("touchstart", function(evt) {
+        if (tapFlag) {
+            evt.preventDefault();
+        }
+    }, true);
+
+    doc.body.addEventListener("touchend", function(evt) {
+        tapFlag = true;
+        clearTimeout(timer);
+        timer = setTimeout(function() {
+            tapFlag = false;
+        }, 200);
+    }, true);
+
+})(window, document);
