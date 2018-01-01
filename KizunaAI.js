f3ee663ada716a4d7ab1065ab579267c15dfea2e
松本将輝 <Air@MacBook-Pro.local>
2018-01-01 12:22:06 +0900

    test

--- KizunaAI.js
+++ KizunaAI.js
@@ -12,16 +12,3 @@ function sound()
 	// [ID:sound-file]の音声ファイルを再生[play()]する
 	document.getElementById( id ).play() ;
 }
-
-/ダブルタップの挙動制御/
-<script>
-/* ダブルタップによる拡大を禁止 */
-var t = 0;
-  document.documentElement.addEventListener('touchend', function (e) {
-  var now = new Date().getTime();
-  if ((now - t) < 350){
-    e.preventDefault();
-  }
-  t = now;
-  }, false);
-</script>
